a0bd637189853190a39b411882a332f4d846d7f3
ciberseguranca <carlosfurtacorreia@enta.pt>
2022-09-22 12:06:44 +0000

    Implementa várias operações aritméticas

--- programa1.py
+++ programa1.py
@@ -5,12 +5,28 @@ Este programa implementa funções aritméticas
 '''
 
 
-def somar(valor1, valor2):
-    total = valor1 + valor2
+def aritmetica(valor1, valor2, op='+'):
+    """
+    Esta função implementa as operações de somar, subtrair, multiplicar e dividir
+    :param valor1: Primeiro fator da operação
+    :param valor2: Segundo fator de operação
+    :param op: Operação; valores válidos são: + - : *
+    :return: Resultado da operação
+    """
+    total = 'Código de operação inválido'
+    if op =='+':
+        total = valor1 + valor2
+    if op =='-':
+        total = valor1 - valor2
+    if op == ':':
+        total = valor1 / valor2
+    if op =='*':
+        total = valor1 * valor2
+
     return total
 
 
 if __name__ == '__main__':
     fator1 = 10
     fator2 = 20
-    print(f'{fator1} mais {fator2} = {somar(fator1, fator2)}')
+    print(f'{fator1} mais {fator2} = {aritmetica(fator1, fator2, "*")}')
