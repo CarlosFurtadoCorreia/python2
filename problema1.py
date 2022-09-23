eff25b5a5c5aaf4cd21e2e9afd09ae1bb4350918
ciberseguranca <carlosfurtacorreia@enta.pt>
2022-09-23 12:12:25 +0000

    problemas feito na aula

--- problema1.py
+++ problema1.py
@@ -1,5 +1,23 @@
 '''
 1. Pede ao utilizador 2 numeros o 2º numero deve ser >= 1º
-    mostra todos os numeros primos, depois de mostrar os numeros
-    diz quantos numeros primos havia.
+    mostra todos os numeros primos que há entre o 1º e o 2º,
+    depois de mostrar os numeros diz quantos numeros primos
+     que havia.
 '''
+
+def primos(num1, num2):
+    zeros = 0
+    for n in range(num1, num2 + 1):
+        if num2 % n == 0:
+            zeros += 1  # zeros = zeros + 1
+    return zeros
+
+
+if __name__ == '__main__':
+    continuar = 's'
+    while continuar == 's':
+        num1 = int(input('Insira um primeiro número: '))
+        num2 = int(input('Insira um segundo número (> que o primeiro numero): '))
+        print(f'{primos(num2)}')
+        continuar = input('Repetir [s] ou [n]? ')
+    print('Adeus!')
