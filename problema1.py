d9c1c5f7539a62c8f804233355723998f3469043
ciberseguranca <carlosfurtacorreia@enta.pt>
2022-09-23 11:51:45 +0000

    problemas

---
+++ problema1.py
@@ -0,0 +1,5 @@
+'''
+1. Pede ao utilizador 2 numeros o 2º numero deve ser >= 1º
+    mostra todos os numeros primos, depois de mostrar os numeros
+    diz quantos numeros primos havia.
+'''
---
+++ problema2.py
@@ -0,0 +1,5 @@
+'''
+2. Pede ao utilizador um numero inicial. Pede ao utilizador
+    um numero que representa "Quantos" mostra aquela quantidade
+    de numeros primos a partir do numero inicial
+'''
