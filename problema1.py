54601511777239e66c6a808fa4da0cab8735f54d
Carlos <carlosfurtadocorreia@enta.pt>
2022-09-24 11:46:08 +0000

    problema1 resolvido

--- problema1.py
+++ problema1.py
@@ -2,22 +2,23 @@
 1. Pede ao utilizador 2 numeros o 2º numero deve ser >= 1º
     mostra todos os numeros primos que há entre o 1º e o 2º,
     depois de mostrar os numeros diz quantos numeros primos
-     que havia.
+    havia.
 '''
 
-def primos(num1, num2):
-    zeros = 0
-    for n in range(num1, num2 + 1):
-        if num2 % n == 0:
-            zeros += 1  # zeros = zeros + 1
-    return zeros
-
-
 if __name__ == '__main__':
+    zeros = 0
     continuar = 's'
     while continuar == 's':
         num1 = int(input('Insira um primeiro número: '))
         num2 = int(input('Insira um segundo número (> que o primeiro numero): '))
-        print(f'{primos(num2)}')
+        for j in range(num1, num2 + 1):
+            if j >= 1:
+                for k in range(2, j):
+                    if j % k == 0:
+                        break
+                else:
+                    print(j)
+                    zeros += 1
+        print(f'Entre o {num1} e o {num2} existem {zeros} números primos')
         continuar = input('Repetir [s] ou [n]? ')
     print('Adeus!')
